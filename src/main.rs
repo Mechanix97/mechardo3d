184b3c0d998b4892279e1f807cffa7c3117364ce
Mechardo <lucas_rack@live.com.ar>
2025-07-28 19:09:50 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
