51200e2014ee460b85c68ab27cc6145a00b9635f
Mechardo <lucas_rack@live.com.ar>
2025-07-30 11:25:22 -0300

    Init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./templates/**/*.{html,tera}" // Scan Tera templates
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
