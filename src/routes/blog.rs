4483a14e8a72c0b2970b68be4760f8ea2ab796ca
Mechardo <lucas_rack@live.com.ar>
2025-07-31 14:08:16 -0300

    Add blog html

--- src/routes/blog.rs
+++ src/routes/blog.rs
@@ -3,7 +3,7 @@ use axum::{Extension, extract::Path, response::Html};
 use tera::{Context, Tera};
 
 pub async fn blog(Extension(tera): Extension<Tera>) -> Html<String> {
-    let posts = get_posts().expect("Error loading post");
+    let posts = get_posts().expect("Error loading posts");
     let mut context = Context::new();
     context.insert("title", "Blog");
     context.insert("posts", &posts);
@@ -16,11 +16,22 @@ pub async fn blog(Extension(tera): Extension<Tera>) -> Html<String> {
 
 pub async fn blog_post(Extension(tera): Extension<Tera>, Path(id): Path<String>) -> Html<String> {
     let posts = get_posts().expect("Error loading posts");
-    let post = posts.iter().find(|p| p.id == id).expect("Post not found");
+    let post: &crate::models::blog_post::BlogPost = posts
+        .iter()
+        .find(|p: &&crate::models::blog_post::BlogPost| p.id == id)
+        .expect("Post not found");
+
+    // Seleccionar hasta 2 posts relacionados (excluyendo el post actual)
+    let related_posts: Vec<_> = posts
+        .iter()
+        .filter(|p| p.id != id) // Excluir el post actual
+        .take(2) // Limitar a 2 posts
+        .collect();
 
     let mut context = Context::new();
     context.insert("title", &post.title);
     context.insert("post", &post);
+    context.insert("related_posts", &related_posts);
 
     let rendered = tera
         .render("blog_post.html", &context)
