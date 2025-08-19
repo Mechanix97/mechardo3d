5be3e94963f4f068dfd22e42a3d595723e24a3dd
Mechardo <lucas_rack@live.com.ar>
2025-08-19 16:30:17 -0300

    Modify blog posts

--- src/models/blog_post.rs
+++ src/models/blog_post.rs
@@ -1,9 +1,26 @@
-use serde::{Deserialize, Serialize};
+use chrono::{DateTime, NaiveDate, Utc};
+use serde::{Deserialize, Deserializer, Serialize};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct BlogPost {
     pub id: String,
     pub title: String,
-    pub content: String,
-    pub date: String,
+    pub summary: Option<String>,
+    pub route: Option<String>,
+    pub thumbnail: Option<String>,
+    #[serde(deserialize_with = "deserialize_date")]
+    pub date: DateTime<Utc>,
+}
+
+fn deserialize_date<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let naive_date = NaiveDate::parse_from_str(&s, "%d-%m-%Y").map_err(serde::de::Error::custom)?;
+    let datetime = naive_date
+        .and_hms_opt(0, 0, 0)
+        .ok_or_else(|| serde::de::Error::custom("Invalid date"))?
+        .and_utc();
+    Ok(datetime)
 }
--- src/routes/blog.rs
+++ src/routes/blog.rs
@@ -1,9 +1,14 @@
 use crate::data::blog_data::get_posts;
 use axum::{Extension, extract::Path, response::Html};
+use rand::rng;
+use rand::seq::IteratorRandom;
+use std::fs;
 use tera::{Context, Tera};
 
 pub async fn blog(Extension(tera): Extension<Tera>) -> Html<String> {
-    let posts = get_posts().expect("Error loading posts");
+    let mut posts = get_posts().expect("Error loading posts");
+    posts.sort_by(|a, b| b.date.cmp(&a.date));
+
     let mut context = Context::new();
     context.insert("title", "Blog");
     context.insert("posts", &posts);
@@ -21,16 +26,23 @@ pub async fn blog_post(Extension(tera): Extension<Tera>, Path(id): Path<String>)
         .find(|p: &&crate::models::blog_post::BlogPost| p.id == id)
         .expect("Post not found");
 
-    // Seleccionar hasta 2 posts relacionados (excluyendo el post actual)
-    let related_posts: Vec<_> = posts
-        .iter()
-        .filter(|p| p.id != id) // Excluir el post actual
-        .take(2) // Limitar a 2 posts
-        .collect();
-
     let mut context = Context::new();
     context.insert("title", &post.title);
     context.insert("post", &post);
+
+    if let Some(post_content_route) = &post.route {
+        let content = fs::read_to_string(format!("templates/blog/{}", post_content_route))
+            .expect("Post content not found");
+        context.insert("content", &content);
+    } else {
+        context.insert("content", &post.summary);
+    }
+
+    let related_posts: Vec<_> = posts
+        .iter()
+        .filter(|p| p.id != id)
+        .choose_multiple(&mut rng(), 2);
+
     context.insert("related_posts", &related_posts);
 
     let rendered = tera
